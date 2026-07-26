55debb3ea7b7d9383ab70585bbe226f742505ad5
Subha Kundu <subhakundu@Subhas-MacBook-Air.local>
2026-07-26 15:21:49 +0530

    stw day 01 draft

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log("hello , world");
+console.log(32);
+console.log("the value of the 32 is :",32);
+console.log(6*2)
+console.log(8*9)
+console.log(`Hare Krishna
+    This is the Gurukulam's STW`)
+console.log('5' + '3')
+console.log('5' - '3')
+console.log('5'==5) /* this is true , double qeual meain it only see the value*/
+console.log('5'===5)/* triple equal meain original things concider*/
+console.log('This is a text , the value of 2 + 3 : ${2+3}')
+const name ="Gurukulam's STW"
+console.log('Hey', name, "who are you?" )
+let a=6;
+let b=8;
+console.log(a+b)
+a=10
+console.log(a+b)
+console.log(typeof a)
+console.log(typeof name)
+a='Hello'/*this is applicaple in js due to it is not differ but name=123 this is wrong*/
+const arr=[1,2,3,4,5]
+console.log(arr)
+console.log(arr[1])
+const input=prompt('Enter your name')
+console.log('your name is :', input)
+console.log(typeof arr)
+
+const obj ={
+name:"Subha",
+age :20,
+isStudet: true 
+}
+console.log(typeof obj == typeof arr)
+
+const abc=null;
+const xyz= undefined;
+console.log(abc==xyz)
+console.log(xyz)
+console.log(obj.name)
+console.log(obj.collage)
+console.log(obj.school.name)
